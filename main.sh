642cc8b25bfb87179c679087e83827c442df47c2
Pablo Saquilan <djmankeletor@gmail.com>
2026-03-01 09:18:37 -0300

    feat: Update MATE desktop background and terminal profile configurations, and bump version to 0.99rc1.

--- main.sh
+++ main.sh
@@ -2,7 +2,7 @@
 # main.sh - Orquestador ESFP Córdoba ISO Customizer
 # Licencia: GNU GPL v3
 # Filosofía: KISS / Modular
-VERSION="0.99rc"
+VERSION="0.99rc1"
 
 # 1. Cargar Configuración
 if [ ! -f ./config.env ]; then
