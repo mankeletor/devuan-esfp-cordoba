b74e3cc5e62207b33c217c03b3453d208cf372bf
Pablo Saquilan <djmankeletor@gmail.com>
2026-03-01 09:04:55 -0300

    chore: Update version to 0.99rc.

--- main.sh
+++ main.sh
@@ -2,7 +2,7 @@
 # main.sh - Orquestador ESFP Córdoba ISO Customizer
 # Licencia: GNU GPL v3
 # Filosofía: KISS / Modular
-VERSION="0.99"
+VERSION="0.99rc"
 
 # 1. Cargar Configuración
 if [ ! -f ./config.env ]; then
