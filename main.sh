b712dbc510cb673a26522bb8b3f954b35565aa86
Pablo Saquilan <djmankeletor@gmail.com>
2026-03-01 22:29:20 -0300

    feat: Bump version to 0.99rc15 and remove a comment from preseed.cfg.

--- main.sh
+++ main.sh
@@ -2,7 +2,7 @@
 # main.sh - Orquestador ESFP Córdoba ISO Customizer
 # Licencia: GNU GPL v3
 # Filosofía: KISS / Modular
-VERSION="0.99rc14"
+VERSION="0.99rc15"
 
 # 1. Cargar Configuración
 if [ ! -f ./config.env ]; then
