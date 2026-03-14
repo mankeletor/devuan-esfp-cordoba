246e2e9af2ba3128c0b46f6a565376e8a70c6887
Pablo Saquilan <djmankeletor@gmail.com>
2026-03-14 09:19:43 -0300

    chore: Remove obsolete ISO customization scripts, preseed configurations, and package lists.

--- test-xsession.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p /etc/X11/Xsession.d
-cat > /etc/X11/Xsession.d/99cd-home << 'INNER_EOF'
-# Fix for working directory being / instead of $HOME
-if [ "$PWD" = "/" ] && [ -n "$HOME" ] && [ -d "$HOME" ]; then
-    cd "$HOME"
-fi
-INNER_EOF
